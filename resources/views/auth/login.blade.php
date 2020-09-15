c8ae1494839563f1bf47a5e4412148032bed864a
Florent <pelletier.ft1@gmail.com>
2020-09-15 09:41:59 +0200

    login UI FINAL

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
   <link rel="icon" type="image/png" href="../front/images/logo-glpmr.svg">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
   <title>
-  {{ __('Authentification') }}
+    {{ __('Authentification') }}
   </title>
   <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
   <!--     Fonts and icons     -->
@@ -51,97 +51,99 @@
       <div class="container">
         <div class="col-md-4 ml-auto mr-auto">
           <div class="card card-login card-plain">
-              <div class="card-header text-center">
-                  <img src="../front/images/logo-glpmr.svg" alt="">
+            <div class="card-header text-center">
+              <img src="../front/images/logo-glpmr.svg" alt="">
+            </div>
+            <div class="card-body">
+              <form method="POST" action="{{ route('login') }}">
+                @csrf
+                <!-- Add -->
+                <div class="input-group no-border input-lg">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text">
+                      <i class="now-ui-icons users_circle-08"></i>
+                    </span>
+                  </div>
+                  <input type="text" placeholder="E-mail" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                  @error('email')
+                  <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                  </span>
+                  @enderror
+                </div>
+                <div class="input-group no-border input-lg">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text">
+                      <i class="now-ui-icons text_caps-small"></i>
+                    </span>
+                  </div>
+                  <input type="password" placeholder="Mot de passe" class="form-control" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                  @error('password')
+                  <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                  </span>
+                  @enderror
+                </div>
+            </div>
+
+            <!-- Add -->
+            <div class="card-footer text-center">
+              <button type="submit"  class="btn btn-primary btn-round btn-lg btn-block">{{ __('Connexion') }}</button>
+              <div class="pull-right">
+                @if (Route::has('password.request'))
+                <h6> <a class="link" href="{{ route('password.request') }}">
+                    {{ __('Mot de passe oublié?') }}
+                  </a></h6>
+                @endif
               </div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+              </form>
+            </div>
+          </div>
 
-                        <div class="form-group row">
-                    <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Email') }}</label>
-                    <div class="col-md-6">
-
-                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-        </div>
-    </div>
-</div>
-<footer class="footer">
-      <div class=" container ">
-        <nav>
-          <ul>
-            <li>
-              <a href="http://www.pasteurmontroland.com/">
-                Lycée Pasteur Mont Roland Dole
-              </a>
-            </li>
-            <li>
-              <a href="">
-                A propos
-              </a>
-            </li>
-          </ul>
-        </nav>
-        <div class="copyright" id="copyright">
-          &copy;
-          <script>
-            document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()))
-          </script>
-          Coded by <a href="" target="_blank">Damien, Hugo et Florent</a>.
-        </div>
+        
       </div>
-    </footer>
+    </div>
   </div>
-  <!--   Core JS Files   -->
-  <script src="../front/js/core/jquery.min.js" type="text/javascript"></script>
-  <script src="../front/js/core/popper.min.js" type="text/javascript"></script>
-  <script src="../front/js/core/bootstrap.min.js" type="text/javascript"></script>
-  <!--  Plugin for Switches, full documentation here: http://www.jque.re/plugins/version3/bootstrap.switch/ -->
-  <script src="../front/js/plugins/bootstrap-switch.js"></script>
-  <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
-  <script src="../front/js/plugins/nouislider.min.js" type="text/javascript"></script>
-  <!--  Plugin for the DatePicker, full documentation here: https://github.com/uxsolutions/bootstrap-datepicker -->
-  <script src="../front/js/plugins/bootstrap-datepicker.js" type="text/javascript"></script>
-  <!--  Google Maps Plugin    -->
-  <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
-  <!-- Control Center for Now Ui Kit: parallax effects, scripts for the example pages etc -->
-  <script src="../front/js/now-ui-kit.js?v=1.3.0" type="text/javascript"></script>
+
+      <footer class="footer">
+        <div class=" container ">
+          <nav>
+            <ul>
+              <li>
+                <a href="http://www.pasteurmontroland.com/">
+                  Lycée Pasteur Mont Roland Dole
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  A propos
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div class="copyright" id="copyright">
+            &copy;
+            <script>
+              document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()))
+            </script>
+            Coded by <a href="" target="_blank">Damien, Hugo et Florent</a>.
+          </div>
+        </div>
+      </footer>
+    </div>
+    <!--   Core JS Files   -->
+    <script src="../front/js/core/jquery.min.js" type="text/javascript"></script>
+    <script src="../front/js/core/popper.min.js" type="text/javascript"></script>
+    <script src="../front/js/core/bootstrap.min.js" type="text/javascript"></script>
+    <!--  Plugin for Switches, full documentation here: http://www.jque.re/plugins/version3/bootstrap.switch/ -->
+    <script src="../front/js/plugins/bootstrap-switch.js"></script>
+    <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+    <script src="../front/js/plugins/nouislider.min.js" type="text/javascript"></script>
+    <!--  Plugin for the DatePicker, full documentation here: https://github.com/uxsolutions/bootstrap-datepicker -->
+    <script src="../front/js/plugins/bootstrap-datepicker.js" type="text/javascript"></script>
+    <!--  Google Maps Plugin    -->
+    <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+    <!-- Control Center for Now Ui Kit: parallax effects, scripts for the example pages etc -->
+    <script src="../front/js/now-ui-kit.js?v=1.3.0" type="text/javascript"></script>
 </body>
