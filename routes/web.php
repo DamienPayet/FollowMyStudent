eaa37d65b4c77a27084dbb94efc081bf2f5406de
payet_d <damien39.payet@outlook.fr>
2020-09-24 09:24:10 +0200

    Route.

--- routes/web.php
+++ routes/web.php
@@ -15,56 +15,42 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', function () {
-    return view('front.index');
+  return view('front.index');
 })->name('index')->middleware('auth');
-//Route::get('front/index', 'OffreController@in')->name('front.offre');
 
-//Route redirection vers questionnaire
-Route::get('front/questionnaire', 'StudentFrontController@questionnaire')->name('questionnaire');
-Route::get('front/questions', 'StudentFrontController@questions')->name('questions');
-Route::get('front/end_question', 'StudentFrontController@end_question')->name('end_question');
-Route::get('front/response_store', 'StudentFrontController@response_store')->name('response_store');
-//Route redirection vers forum acceuil
-Route::get('front/forum', 'StudentFrontController@forum')->name('forum');
-//Route redirection vers forum mes sujet
-Route::get('front/mes_sujets', 'StudentFrontController@forum_mes_sujets')->name('forum_mesSujets');
-//Route redirection vers les offres
-Route::get('front/offres', 'StudentFrontController@offre')->name('offre');
+
+//Route pour les admin :
+Route::group(['middleware' => 'admin'], function () {
+  //route admin gestion user
+  Route::resource('back/users', UserController::class);
+  //route admin gestion offre
+  Route::resource('back/offre','OffreController');
+  
+  Route::get('/back1', function () {return view('layouts.templateBack');})->name('back');
+  Route::get('/testt', function () {return view('test');})->name('aaa');
+
+  Route::get('/home', 'HomeController@index')->name('home');
+
+  //Route BACK
+  Route::get('/testBack', function () {return view('layouts.templateBack');});
+});
 
 Route::group(['middleware' => 'auth'], function () {
-  //Route::resource('front/offre','OffreController');
-  Route::get('front/questionnaire', 'StudentFrontController@questionnaire')->name('questionnaire');
-  Route::get('front/questions', 'StudentFrontController@questions')->name('questions');
-  Route::get('front/end_question', 'StudentFrontController@end_question')->name('end_question');
-  Route::get('front/response_store', 'StudentFrontController@response_store')->name('response_store');
   //Route redirection vers forum acceuil
   Route::get('front/forum', 'StudentFrontController@forum')->name('forum');
   //Route redirection vers forum mes sujet
   Route::get('front/mes_sujets', 'StudentFrontController@forum_mes_sujets')->name('forum_mesSujets');
-  //Route redirection vers les offres
-  Route::get('front/offres', 'StudentFrontController@offre')->name('offre');
-  Route::resource('back/offre','OffreController');
+  //Route  chat direct
   Route::get('ajaxRequest', 'StudentFrontController@ajaxRequest')->name('ajaxRequest.index');
   Route::get('tata', 'StudentFrontController@ajaxRequest1')->name('ajaxRequest.index');
   Route::post('ajaxRequest/conv', 'StudentFrontController@ajaxRequestSync')->name('ajaxRequest.sync');
   Route::post('ajaxRequest', 'StudentFrontController@ajaxRequestPost')->name('ajaxRequest.post');
-  Route::get('front/offre', 'OffreFrontController@index')->name('offre_front_index');
+  //Route redirection vers front offre
+  Route::get('front/offres', 'OffreFrontController@index')->name('offre_front_index');
   Route::get('front/offre/{offre}', 'OffreFrontController@show')->name('offre_front_show');
-
+  //Route redirection vers questionnaire
+  Route::get('front/questionnaire', 'StudentFrontController@questionnaire')->name('questionnaire');
+  Route::get('front/questions', 'StudentFrontController@questions')->name('questions');
+  Route::get('front/end_question', 'StudentFrontController@end_question')->name('end_question');
+  Route::get('front/response_store', 'StudentFrontController@response_store')->name('response_store');
 });
-
-Route::get('/back1', function () {
-    return view('layouts.templateBack');
-})->name('back');
-Route::get('/testt', function () {
-  return view('test');
-})->name('aaa');
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-//Route BACK
-Route::get('/testBack', function () {
-  return view('layouts.templateBack');
-});
-//Route Gestion User
-Route::resource('back/users', UserController::class);
