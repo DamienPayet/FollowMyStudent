55513aac57b453c7ff4589205e503e3075b5cfcb
Florent <pelletier.ft1@gmail.com>
2021-02-04 17:00:44 +0100

    TEST

--- resources/views/back/log/index.blade.php
+++ resources/views/back/log/index.blade.php
@@ -18,49 +18,67 @@
                         <h4 class="card-title ">Tout les logs</h4>
                     </div>
                     <div class="card-body">
-                        <table id="table_id" class="table">
-                            <thead>
-                                <tr>
-                                    <th>id</th>
-                                    <th>Action</th>
-                                    <th>URL</th>
-                                    <th>Méthode</th>
-                                    <th>Adresse IP</th>
-                                    <th width="300px">User Agent</th>
-                                    <th>Utilisateur</th>
-                                    <th>Date</th>
-                                    <th>Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach($logs as $log)
-                                <tr>
-                                    <td>{{$log->id}}</td>
-                                    <td>{{ $log->subject }}</td>
-                                    <td class="text-success">{{ $log->url }}</td>
-                                    <td><label class="label label-info">{{ $log->method }}</label></td>
-                                    <td class="text-warning">{{ $log->ip }}</td>
-                                    <td class="text-danger">{{ $log->agent }}</td>
-                                    <td>{{$log->user_id}}</td>
-                                    <td>{{$log->created_at}}</td>
-                                    <td>
-                                        <div style="display: inline-flex;">
-                                            <form action="{{route('log.destroy', $log->id)}}" method="post">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit" rel="tooltip" class="btn  btn-linght btn-round" onclick="return confirm('Est tu sur de vouloir supprimer ce log ?')">
-                                                    <i class="fas fa-times"></i>
-                                                </button>
-                                            </form>
-                                        </div>
-                                    </td>
-                                </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
+                        @if (session('status'))
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <div class="alert alert-success">
+                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                        <span aria-hidden="true">
+                                            <i class="fas fa-times"></i>
+                                        </span>
+                                    </button>
+                                    <span>{{ session('status') }}</span>
+                                </div>
+                            </div>
+                        </div>
+                        @endif
+                        <div class="table-responsive">
+
+                            <table id="table_id" class="table">
+                                <thead>
+                                    <tr>
+                                        <th>id</th>
+                                        <th>Action</th>
+                                        <th>URL</th>
+                                        <th>Méthode</th>
+                                        <th>IP</th>
+                                        <th width="300px">User Agent</th>
+                                        <th>Utilisateur</th>
+                                        <th>Date</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach($logs as $log)
+                                    <tr>
+                                        <td>{{$log->id}}</td>
+                                        <td>{{ $log->subject }}</td>
+                                        <td class="text-success">{{ $log->url }}</td>
+                                        <td><label class="label label-info">{{ $log->method }}</label></td>
+                                        <td class="text-warning">{{ $log->ip }}</td>
+                                        <td class="text-danger">{{ $log->agent }}</td>
+                                        <td>{{$log->user_id}}</td>
+                                        <td>{{$log->created_at}}</td>
+                                        <td>
+                                            <div style="display: inline-flex;">
+                                                <form action="{{route('log.destroy', $log->id)}}" method="post">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" rel="tooltip" class="btn  btn-linght btn-round" onclick="return confirm('Est tu sur de vouloir supprimer ce log ?')">
+                                                        <i class="fas fa-times"></i>
+                                                    </button>
+                                                </form>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    @endsection
+</div>
+@endsection
